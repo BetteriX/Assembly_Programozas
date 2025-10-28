e6557ccdc5db0066b96344ebeb5cf194e6d1f1d4
BetteriX <szabozsolt430@gmail.com>
2025-10-28 10:26:16 +0100

    6ora fix

--- 6/fib.s
+++ 6/fib.s
@@ -4,25 +4,24 @@
 main:
     push    rbp
     mov     rbp, rsp
-    sub     rsp, 56
+    sub     rsp, 52
+
     mov     DWORD PTR[rbp-52], 0    #   F[0]
     mov     DWORD PTR[rbp-48], 1    #   F[1]
     mov     rdx, 2
-    jmp     cond
-
+cond:
+    cmp     rdx, 13
+    jge     done
 body:
-    mov     ebx, DWORD PTR[rbp-56+rdx*4-4] # 48
-    mov     ecx, DWORD PTR[rbp-56+rdx*4-8] # 
+    mov     ebx, DWORD PTR[rbp-52+rdx*4-4]
+    mov     ecx, DWORD PTR[rbp-52+rdx*4-8]
 
     add     ebx, ecx
 
-    mov     DWORD PTR[rbp-56-i*4], ebx # 
-    inc     DWORD PTR[rbp-56]
-
-cond:
-    cmp     DWORD PTR[rbp-56], 13
-    jl      body
-
+    mov     DWORD PTR[rbp-52+rdx*4], ebx
+    inc     rdx
+    jmp     cond
+done:
     mov     eax, DWORD PTR[rbp-4]
     mov     rsp, rbp
     pop     rbp
